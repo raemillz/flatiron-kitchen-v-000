a5b2ec2c130e8cbc8534318ba68147bf1e3e34e6
Rachel Miller <raeanne1991@gmail.com>
2018-04-10 16:05:53 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -17,6 +17,10 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.find(params[:id])
   end
 
+  def update
+
+  end
+
   def show
     @ingredient = Ingredient.find(params[:id])
   end
